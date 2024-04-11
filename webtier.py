722bb90f099a675ab77815aaae336824e4751188
chanvitj <chanvitj@yahoo.com>
2024-04-11 09:27:03 +0700

    Update webtier.py

--- webtier.py
+++ webtier.py
@@ -6,16 +6,18 @@ apptier = Flask(__name__)
 
 @apptier.route('/')
 def hello():
-  url = "http://192.168.0.140:8001"
-  headers = {'content_type': 'application/json'}
-  method = 'GET'
-  response = result = requests.request(method, url, headers=headers)
-  if response.status_code == 200:
-    data = response.json()
-    data['additional information']='This is additional information from Web-Tier.'
-    return data 
-  else:
-    return f"Error: {response.status_code}"
+  r = {"id": 1, "title": "This is JSON data from App-Tier."}
+  return r 
+#  url = "http://192.168.0.140:8001"
+#  headers = {'content_type': 'application/json'}
+#  method = 'GET'
+#  response = result = requests.request(method, url, headers=headers)
+#  if response.status_code == 200:
+#    data = response.json()
+#    data['additional information']='This is additional information from Web-Tier.'
+#    return data 
+#  else:
+#    return f"Error: {response.status_code}"
 
 if __name__ == '__main__':
   apptier.run(host='0.0.0.0', port=8000)
